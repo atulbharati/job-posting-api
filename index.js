af833a1552ee3790744400ceb0db94b830ce54f0
Atul Bharati <atul.bharati.2010@gmail.com>
2019-04-13 01:26:42 -0400

    updated index.js

--- index.js
+++ index.js
@@ -179,6 +179,6 @@ app.get('/nonTechnical', function(req, res) {
     });
 });
 
-app.listen(3000, function() {
+app.listen(process.env.PORT || 3000, function() {
     console.log('Listening!');
 });
